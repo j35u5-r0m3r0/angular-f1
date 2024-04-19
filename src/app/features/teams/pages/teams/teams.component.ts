435141edda55e67330a8c15c1384fd307f9b043d
Romero Clairac <jesus.romero-clairac@capgemini.com>
2024-04-19 14:58:01 +0200

    feature team

--- src/app/features/teams/pages/teams/teams.component.ts
+++ src/app/features/teams/pages/teams/teams.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.css']
 })
-export class TeamsComponent {
+export class TeamsComponent implements OnInit {
+
+  teams: any = {};
+  
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.getCurrentTeamStandings();
+  }
+
+  getCurrentTeamStandings(){
+    this.apiService.getCurrentTeamStandings().subscribe((resp:any) => {
+      this.teams = resp.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+      
+      });
+    
+  }
+
+  openConstructor(constructorId:number){
+    console.log("constructorId=",constructorId);
+    this.router.navigate([`./teams/${constructorId}`]);
+  }
 
 }
